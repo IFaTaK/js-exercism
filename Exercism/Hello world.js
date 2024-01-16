9238b3b962e881da3b11356cccd28d4c40f4a615
Kilian LEFEVRE <kilian.lefevre@outlook.fr>
2024-01-17 00:59:26 +0100

    The 6 first exercises from Exercism.io

---
+++ Exercism/Hello world.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+
+const hello = () => {
+  return "Hello, World!";
+}
+
+console.log(hello());
+
+function hello2() {
+  return "Hello, World!";
+}
+
+console.log(hello2());
